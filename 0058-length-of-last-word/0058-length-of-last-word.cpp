aef60e3cc0054a30b5d050706a74e7f7a7ec2799
Shiveshwar kumar Sah <149902164+Shiv00012@users.noreply.github.com>
2024-09-21 23:58:19 +0530

    Time: 4 ms (47.40%), Space: 8 MB (66.86%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -4,12 +4,10 @@ public:
     int length = 0;
     int i = s.length() - 1;
     
-    // Skip trailing spaces
     while (i >= 0 && s[i] == ' ') {
         i--;
     }
     
-    // Count the length of the last word
     while (i >= 0 && s[i] != ' ') {
         length++;
         i--;
