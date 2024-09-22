e32f617ef07f7280740567e930f37837e954b179
Shiveshwar kumar Sah <149902164+Shiv00012@users.noreply.github.com>
2024-09-22 23:41:04 +0530

    Time: 0 ms (100.00%), Space: 8.7 MB (9.13%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        double z = pow(x, n);
-        return z;
+        double result = pow(x, n);
+        return result;
     }
 };
