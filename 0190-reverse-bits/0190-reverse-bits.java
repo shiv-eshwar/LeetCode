79fbcf1f582e18483ea1e24171e4f7642e65109a
Shiveshwar kumar Sah <149902164+Shiv00012@users.noreply.github.com>
2024-10-07 23:48:38 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (45.81%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -0,0 +1,15 @@
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+     int reversed = 0;
+
+        for (int i = 0; i < 32; i++) {
+            // Get the last bit of n
+            int bit = (n >> i) & 1;
+            // Add it to the reversed integer
+            reversed |= (bit << (31 - i));
+        }
+
+        return reversed;   
+    }
+}
