64501322c11611316727226d2d24167cac43091f
Shiveshwar kumar Shah <149902164+shiv-eshwar@users.noreply.github.com>
2025-07-24 22:23:11 +0530

    Time: 1 ms (68.86%), Space: 18.7 MB (51.31%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,9 +1,8 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         n= len(nums)
-        xor = 0
-        for i in range(n+1):
-            xor ^= i
-        for num in nums:
-            xor ^=num
-        return xor
+        exp_sum = n*(n+1)//2 # this is tyhe formula for the sum of n natuiral number 
+        act_sum = sum(nums)
+        return (exp_sum -act_sum)
+
+        ()
