5d0160346bcf11808bc22bec1cebe633358fccae
Shiveshwar kumar Shah <149902164+shiv-eshwar@users.noreply.github.com>
2025-07-29 01:41:15 +0530

    Time: 7 ms (32.86%), Space: 20.1 MB (6.8%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,9 +1,17 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        sets = set()
-        for i,num in enumerate(nums):
-            if num in sets:
-                sets.remove(num)
-            else:
-                sets.add(num)
-        return sets.pop()
+        # sets = set()
+        # for i,num in enumerate(nums):
+        #     if num in sets:
+        #         sets.remove(num)
+        #     else:
+        #         sets.add(num)
+        # return sets.pop()
+        sumofunique = sum(set(nums))
+        total =0
+
+        for num in nums:
+            total += num
+            res = 2* sumofunique -total
+
+        return res
