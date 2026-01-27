c5c72a2bb6cadba7f12ef7a59560a8a8203aebe0
Shiveshwar kumar Shah <149902164+shiv-eshwar@users.noreply.github.com>
2026-01-27 21:46:35 +0530

    Time: 0 ms (100%), Space: 31.5 MB (7.65%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -3,7 +3,7 @@ public:
     int search(vector<int>& nums, int target) {
         int l =0, r= nums.size()-1; int ans;
         while(l<=r){
-            int mid = l+ (r-l)/2;
+            int mid = l+ (r-l)/2; // here we break the array into two half and find the mid so if the target == mid then we are returning mid else if the mid > target then r= mid-1 else l- mid +1, and if not we are returning -1;
 
             if (nums[mid]== target){
                 return mid;
