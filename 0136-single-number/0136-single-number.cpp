86129c58cb1f8775c83916168f90586a5378a698
Shiveshwar kumar Shah <149902164+shiv-eshwar@users.noreply.github.com>
2026-01-28 17:16:37 +0530

    Time: 3 ms (24.56%), Space: 20.6 MB (57.6%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -3,7 +3,7 @@ public:
     int singleNumber(vector<int>& nums) {
         int ans = 0;
         int n= nums.size();
-        for (int i=0; i < n; i++){
+        for (int i=0; i < n; i++){ // here we use the bitwise xor as initilize with the ans as 0 and we know that as if we do the xor with same number it will give 0 else gives same number if done with the 0
             ans = ans^nums[i];
         }
         return ans;
