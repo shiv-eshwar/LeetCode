d6d8d7e9c8cecf6620c787ade28e405792be92dd
Shiveshwar kumar Shah <149902164+shiv-eshwar@users.noreply.github.com>
2026-02-07 19:37:45 +0530

    Time: 3 ms (62.46%), Space: 20.4 MB (42.41%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -2,11 +2,17 @@ class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         n= len(nums)
         
-        x= 0
-        for i in range(0,n+1):
-            x = x ^ i
-        y = 0
-        for i in range(0,n):
-            y = y ^ nums[i]
+        # x= 0
+        # for i in range(0,n+1):
+        #     x = x ^ i
+        # y = 0
+        # for i in range(0,n):
+        #     y = y ^ nums[i]
 
-        return x^y
+        # return x^y  # here we are just doing the xor for 1-n and then for the xor for value and at last doing x ^ y gives the missing number
+        ans =n
+        for i in range(0,n):
+            ans = ans ^ i
+            ans ^= nums[i]
+        
+        return ans
