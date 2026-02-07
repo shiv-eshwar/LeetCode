939341053394295682c3bb4cc394fd9632971438
Shiveshwar kumar Shah <149902164+shiv-eshwar@users.noreply.github.com>
2026-02-07 19:10:21 +0530

    Time: 2 ms (65.08%), Space: 20.6 MB (24.38%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -5,4 +5,4 @@ class Solution:
 
         arr_sum= sum(nums)
 
-        return(total_sum-arr_sum)
+        return(total_sum-arr_sum) # this is the easiest method but now i will try to do with the bit wise operator;
