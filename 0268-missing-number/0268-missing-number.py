0e731eb8f6a272055041918970077350e4fa7529
Shiveshwar kumar Shah <149902164+shiv-eshwar@users.noreply.github.com>
2026-02-07 19:05:41 +0530

    Time: 0 ms (100%), Space: 20.6 MB (24.38%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,8 +1,8 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         n= len(nums)
-        exp_sum = n*(n+1)//2 # this is tyhe formula for the sum of n natuiral number 
-        act_sum = sum(nums)
-        return (exp_sum -act_sum)
+        total_sum = (n*(n+1))//2
 
-        ()
+        arr_sum= sum(nums)
+
+        return(total_sum-arr_sum)
